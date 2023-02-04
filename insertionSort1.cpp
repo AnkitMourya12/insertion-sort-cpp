fe324e6871c9e2f8ea707e7639842b0212391046
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2023-02-05 00:06:08 +0530

    Add files via upload

---
+++ insertionSort1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,j,k,N;
+	cout<<"enter the size of array:"<<endl;
+	cin>>N;
+	int a[N];
+	cout<<"enter the 5 element of array:"<<endl;
+	for(i=0;i<N;i++)
+	{
+		cin>>a[i];
+	}
+	for(i=0;i<N;i++)
+	{
+		k=a[i];
+		j=i-1;
+		while(j>=0&&a[j]>k)
+		{
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=k;
+	}
+	cout<<"^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;;
+	
+	for(i=0;i<N;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
